6b8eda34b451402614c6a5aea92d6824dbbeeb24
2535633442 <2535633442@qq.com>
2023-12-20 16:51:12 +0800

    no longer need store

--- bookstore/be/model/db_conn.py
+++ bookstore/be/model/db_conn.py
@@ -1,23 +1,23 @@
-import bookstore.database_setup as ds
+from bookstore.database_setup import *
 
 def user_id_exist(user_id):
-    cursor = ds.session.query(ds.User).filter(ds.User.user_id == user_id).first()
-    if (cursor is None):
+    cursor = session.query(User).filter(User.user_id == user_id).first()
+    if cursor is None:
         return False
     else:
         return True
 
 def book_id_exist(store_id, book_id):
-    cursor = ds.session.query(ds.StoreDetail).filter(ds.StoreDetail.store_id == store_id,
-                                                ds.StoreDetail.book_id == book_id).first()
-    if (cursor is None):
+    cursor = session.query(StoreDetail).filter(StoreDetail.store_id == store_id,
+                                                StoreDetail.book_id == book_id).first()
+    if cursor is None:
         return False
     else:
         return True
 
 def store_id_exist(store_id):
-    cursor = ds.session.query(ds.Store).filter(ds.Store.store_id == store_id).first()
-    if (cursor is None):
+    cursor = session.query(Store).filter(Store.store_id == store_id).first()
+    if cursor is None:
         return False
     else:
         return True
